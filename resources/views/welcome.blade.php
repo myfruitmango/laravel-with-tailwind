ac40b0da67b0eb5b384b2ac0a842e2c404b46b1b
mw putri <mwputrimeiliniahp@gmail.com>
2024-02-22 03:06:17 +0700

    updated: add new products

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -148,6 +148,30 @@
                 </div>
             </div>
         </section>
+        <section aria-labelledby="home" class="relative">
+            <div class="flex flex-wrap-reverse gap-8 justify-center">
+                <div class="flex gap-6 fle-wrap flex-col items-start lg:justify-center">
+                    <div class="flex flex-wrap flex-col items-start gap-2">
+                        <h2 class="text-4xl font-bold">Relax, you’re home!</h2>
+                        <p class="max-w-md">
+                            All our supported hardware includes traditional inputs so you can integrate our products
+                            into your house in a way that’s
+                            best for everyone.
+                        </p>
+                        <a href="#"
+                            class="py-2 px-6 bg-amber-400 focus:outline-none focus-visible:ring-4 ring-neutral-900 rounded-md ring-offset-4 ring-offset-white dark:ring-offset-neutral-800 dark:ring-amber-400 my-12 flex gap-2 shadow-xl hover:shadow-none hover:to-neutral-600 transition-shadow dark:text-neutral-900">
+                            Sign Up
+                        </a>
+                    </div>
+                </div>
+                <img src="{{ url('assets/img/table.png') }}" alt="table" />
+            </div>
+            <div
+                class="absolute -bottom-6 -right-32 -z-10 aspect-square md:border-8 border-amber-400 rounded-full md:w-64 lg:w-95 xl:max-w-lg">
+            </div>
+            <div class="bg-teal-900 h-24 -mx-8 mt-12 xl:mt-0 xl:h-48 xl:absolute w-screen -bottom-8 -z-10">
+            </div>
+        </section>
     </main>
 </body>
 
