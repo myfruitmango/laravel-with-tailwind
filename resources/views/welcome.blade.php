8ca938abcf888bbabaf38437bda43396fe43eeb1
mw putri <mwputrimeiliniahp@gmail.com>
2024-02-22 02:41:35 +0700

    updated: new card Qualities

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,6 +73,48 @@
             </svg>
             <span>Download The App</span>
         </a>
+        <section aria-label="qualities" class="relative">
+            <img src="{{ url('assets/img/dots.svg') }}" alt="dots"
+                class="hidden lg:block absolute top-1/2 -translate-y-1/2 -left-24 xl:-left-7"aria-hidden="true" />
+            <img src="{{ url('assets/img/dots.svg') }}" alt="dots"
+                class="hidden lg:block absolute top-1/2 -translate-y-1/2 -right-24 xl:-right-7"aria-hidden="true" />
+            <h2 id="qualities" class="sr-only">Our Qualities</h2>
+            <div
+                class="container mx-auto max-w-5xl flex gap-12 flex-wrap items-start justify-center md:justify-between">
+                <div class="grid gap-4 justify-items-center text-center md:flex-1">
+                    <div class="rounded-full border-8 border-amber-400 p-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-14 h-14">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
+                        </svg>
+                    </div>
+                    <h3 class="text-3xl font-bold">Safe</h3>
+                    <p>Our Products are secure and private out-of-the-box</p>
+                </div>
+                <div class="grid gap-4 justify-items-center text-center md:flex-1">
+                    <div class="rounded-full border-8 border-amber-400 p-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-14 w-14" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M13 10V3L4 14h7v7l9-11h-7z" />
+                        </svg>
+                    </div>
+                    <h3 class="text-3xl font-bold">Efficient</h3>
+                    <p>Feel good about your wallet and the environment</p>
+                </div>
+                <div class="grid gap-4 justify-items-center text-center md:flex-1">
+                    <div class="rounded-full border-8 border-amber-400 p-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-14 w-14" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
+                        </svg>
+                    </div>
+                    <h3 class="text-3xl font-bold">Proven</h3>
+                    <p>Leading the Smart Home world for 10 years</p>
+                </div>
+            </div>
+        </section>
     </main>
 </body>
 
